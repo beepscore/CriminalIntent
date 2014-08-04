7acf60827132119bc4417dd304ddb744854ffd41
beepscore <support@beepscore.com>
2014-08-03 22:03:43 -0700

    In Crime add instance variables and method Crime().

--- app/src/main/java/com/beepscore/android/criminalintent/Crime.java
+++ app/src/main/java/com/beepscore/android/criminalintent/Crime.java
@@ -1,7 +1,17 @@
 package com.beepscore.android.criminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by stevebaker on 8/3/14.
  */
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
 }
