9c42487f56ddf479888f12d39e1389719e62c52b
beepscore <support@beepscore.com>
2014-08-06 22:00:23 -0700

    In CrimeActivity add local variable fm to match tutorial.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,7 @@
 package com.beepscore.android.criminalintent;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,7 +14,8 @@ public class CrimeActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
+            FragmentManager fm = getFragmentManager();
+            fm.beginTransaction()
                     // use CrimeFragment factory method newInstance
                     .add(R.id.container, CrimeFragment.newInstance("foo", "bar"))
                     .commit();
