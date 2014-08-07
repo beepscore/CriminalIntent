3682fefda462e7f1235fad8da65dff73a3303d95
beepscore <support@beepscore.com>
2014-08-06 22:25:41 -0700

    In CrimeActivity.java onCreate revise closer to tutorial.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
@@ -1,23 +1,28 @@
 package com.beepscore.android.criminalintent;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 
 
 public class CrimeActivity extends Activity
-    implements CrimeFragment.OnFragmentInteractionListener {
+        implements CrimeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-        if (savedInstanceState == null) {
-            FragmentManager fm = getFragmentManager();
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.container);
+
+        if (fragment == null) {
+            fragment = CrimeFragment.newInstance("foo", "bar");
             fm.beginTransaction()
                     // use CrimeFragment factory method newInstance
-                    .add(R.id.container, CrimeFragment.newInstance("foo", "bar"))
+                    .add(R.id.container, fragment)
                     .commit();
         }
     }
