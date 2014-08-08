12222a522f9827e6bc44d1c5288bd8c9171fd890
beepscore <support@beepscore.com>
2014-08-07 21:27:08 -0700

    In CrimeFragment.java add mDateButton, mSolvedCheckBox

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeFragment.java
@@ -9,6 +9,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -32,6 +35,9 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
     private OnFragmentInteractionListener mListener;
 
     /**
@@ -86,6 +92,19 @@ public class CrimeFragment extends Fragment {
 
             public void afterTextChanged(Editable c) {
                 // This one too
+            }
+        });
+
+        mDateButton = (Button)rootView.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)rootView.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // set the crime's solved property
+                mCrime.setSolved(isChecked);
             }
         });
 
