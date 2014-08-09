255a789a5ca004eb1c8c80dcfda9445f3e554442
beepscore <support@beepscore.com>
2014-08-08 20:00:25 -0700

    In CrimeFragmentTest add testFormattedDateStringDate0() and testFormattedDateString(). All unit tests pass.

--- app/src/androidTest/java/com/beepscore/android/criminalintent/CrimeFragmentTest.java
+++ app/src/androidTest/java/com/beepscore/android/criminalintent/CrimeFragmentTest.java
@@ -2,6 +2,10 @@ package com.beepscore.android.criminalintent;
 
 import junit.framework.TestCase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by stevebaker on 8/8/14.
  */
@@ -18,4 +22,37 @@ public class CrimeFragmentTest extends TestCase {
         CrimeFragment crimeFragment = CrimeFragment.newInstance("foo", "bar");
         assertNotNull("CrimeFragment null", crimeFragment);
     }
+
+    public void testFormattedDateStringDate0() {
+        CrimeFragment crimeFragment = CrimeFragment.newInstance("foo", "bar");
+
+        // beginning of UNIX epoch
+        Date testDate = new Date(0);
+        
+        String expected = "Wednesday, Dec 31, 1969";
+        String actual = crimeFragment.formattedDateString(testDate);
+        assertEquals("", expected, actual);
+    }
+
+    public void testFormattedDateString() {
+        CrimeFragment crimeFragment = CrimeFragment.newInstance("foo", "bar");
+
+        // set up test by instantiating a test date
+        Date testDate = new Date(0);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        String dateStringYearMonthDay = "2011-12-13";
+
+        try {
+            testDate = sdf.parse(dateStringYearMonthDay);
+        }
+        catch (Exception e) {
+            //The handling for the code
+        }
+
+        // test
+        String expected = "Tuesday, Dec 13, 2011";
+        String actual = crimeFragment.formattedDateString(testDate);
+        assertEquals("", expected, actual);
+    }
+
 }
