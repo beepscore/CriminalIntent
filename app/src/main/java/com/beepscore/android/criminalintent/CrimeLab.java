a19ac4a327af2c017c7c781d46e1c47b7006d936
beepscore <support@beepscore.com>
2014-08-09 16:43:04 -0700

    In CrimeLab add method populateCrimes and call it.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeLab.java
@@ -19,6 +19,7 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
+        populateCrimes();
     }
 
     // returns a singleton globally available within app
@@ -43,4 +44,15 @@ public class CrimeLab {
         }
         return null;
     }
+
+    private void populateCrimes() {
+        for (int i = 0; i < 100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime #" + i);
+            // set even solved, odd not solved
+            c.setSolved(i % 2 == 0);
+            mCrimes.add(c);
+        }
+    }
+
 }
