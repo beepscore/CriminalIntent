8f79977b1e9aabaf6d46ba6ba92d811f97825246
beepscore <support@beepscore.com>
2014-08-10 22:12:09 -0700

    Add CrimeListActivity.java, activity_crime_list.xml, crime_list.xml. Change CrimeListActivity and CrimeActivity from extends Activity to extends SingleFragmentActivity

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeActivity.java
@@ -1,35 +1,17 @@
 package com.beepscore.android.criminalintent;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.net.Uri;
-import android.os.Bundle;
 
 
-public class CrimeActivity extends Activity
+public class CrimeActivity extends SingleFragmentActivity
         implements CrimeFragment.OnFragmentInteractionListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        // FragmentManager manages a list of fragments and a backStack of fragment transactions
-        // http://developer.android.com/reference/android/app/FragmentManager.html
-        FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null) {
-            fragment = CrimeFragment.newInstance("foo", "bar");
-            fm.beginTransaction()
-                    // use CrimeFragment factory method newInstance
-                    .add(R.id.fragmentContainer, fragment)
-                    .commit();
-        }
+    public void onFragmentInteraction(Uri uri) {
     }
 
-    public void onFragmentInteraction(Uri uri) {
+    protected Fragment createFragment() {
+        return CrimeFragment.newInstance("foo", "bar");
     }
 
 }
