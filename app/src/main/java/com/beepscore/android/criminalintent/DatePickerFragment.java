92c364ad1a4bb7005dd777d7a55ac46ad2996b91
beepscore <support@beepscore.com>
2014-08-22 22:07:54 -0700

    In DatePickerFragment add constructer method newInstance with fragment argument date. In CrimeFragment instantiate date picker fragment using newInstance.

--- app/src/main/java/com/beepscore/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/DatePickerFragment.java
@@ -6,10 +6,26 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 
+import java.util.Date;
+
 /**
  * Created by stevebaker on 8/21/14.
  */
 public class DatePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_DATE = "com.beepscore.android.criminalintent.date";
+    private Date mDate;
+
+    // construct fragment with fragment argument date
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
