792fc0be95ddb1453a84a63e9c8a8bd2c89c9851
beepscore <support@beepscore.com>
2014-08-23 11:49:24 -0700

    In DatePickerFragment use a Calendar to parse Date. Use date components year, month, day to set datePicker. Save date to fragment arguments.

--- app/src/main/java/com/beepscore/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/DatePickerFragment.java
@@ -5,8 +5,11 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by stevebaker on 8/21/14.
@@ -29,8 +32,36 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+
+        // Create a Calendar to get the year, month, and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(calendar.YEAR);
+        int month = calendar.get(calendar.MONTH);
+        int day = calendar.get(calendar.DAY_OF_MONTH);
+
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                // Translate year, month, day into a Date object using a calendar
+                mDate = new GregorianCalendar(year, month, day).getTime();
+
+                // Save the date.
+                // If device is rotated, FragmentManager will destroy current fragment instance
+                // and create a new one.
+                // Could use onSavedInstanceState().
+                // Could use retained fragment.
+                // Big Nerd Ranch book says DialogFragment has a bug, retained fragment doesn't work
+                // Here save state by writing to fragment argument
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
