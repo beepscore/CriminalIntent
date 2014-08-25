5f2cb6248745637dade1877afe065c128e0a38c5
beepscore <support@beepscore.com>
2014-08-24 22:02:35 -0700

    In CrimeFragment delete unused imports.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeFragment.java
@@ -2,13 +2,11 @@ package com.beepscore.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
