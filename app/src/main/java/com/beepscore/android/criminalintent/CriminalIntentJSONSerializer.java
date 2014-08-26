0d975fe55a29ab2d22f98382f527eb6c9f9b6d8e
beepscore <support@beepscore.com>
2014-08-25 19:49:39 -0700

    In CriminalIntentJSONSerializer add method loadCrimes()

--- app/src/main/java/com/beepscore/android/criminalintent/CriminalIntentJSONSerializer.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CriminalIntentJSONSerializer.java
@@ -4,8 +4,13 @@ import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -22,6 +27,38 @@ public class CriminalIntentJSONSerializer {
     public CriminalIntentJSONSerializer(Context context, String filename) {
         mContext = context;
         mFilename = filename;
+    }
+
+    public ArrayList<Crime> loadCrimes() throws JSONException, IOException {
+        ArrayList<Crime> crimes = new ArrayList<Crime>();
+        BufferedReader reader = null;
+
+        try {
+            // Open and read the file into a StringBuilder
+            InputStream inputStream = mContext.openFileInput(mFilename);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder jsonString = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                // Line breaks are omitted and irrelevant
+                jsonString.append(line);
+            }
+
+            // Parse the JSON using JSONTokener
+            JSONArray array = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
+            // Build the array of crimes form JSONObjects
+            for (int i = 0; i < array.length(); i++) {
+                crimes.add(new Crime(array.getJSONObject(i)));
+            }
+        } catch (FileNotFoundException e) {
+            // Ignore this one; it happens when starting fresh
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return crimes;
     }
 
     public void saveCrimes(ArrayList<Crime> crimes)
