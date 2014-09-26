95688ba57a55947e9efa8d1da7cc60b8fd70ca92
beepscore <support@beepscore.com>
2014-09-26 00:06:03 -0700

    Add changes from end of Chapter 16 (pages 266--270) up to challenge that I skipped before. Then add Chapter 18 approx pg 283-285.
    Add menu-v11/fragment_crime_list. In CrimeListFragment add onCreateContextMenu
    Reference
    http://forums.bignerdranch.com/viewtopic.php?f=415&t=6364

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeListFragment.java
@@ -1,8 +1,10 @@
 package com.beepscore.android.criminalintent;
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.ContextMenu;
@@ -14,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -50,6 +51,7 @@ public class CrimeListFragment extends ListFragment {
     private CrimeAdapter mAdapter;
 
     private ArrayList<Crime> mCrimes;
+    private boolean mSubtitleVisible;
 
     // TODO: Rename and change types of parameters
     public static CrimeListFragment newInstance() {
@@ -77,14 +79,33 @@ public class CrimeListFragment extends ListFragment {
 
         mAdapter = new CrimeAdapter(mCrimes);
         setListAdapter(mAdapter);
+        setRetainInstance(true);
+        mSubtitleVisible = false;
     }
 
+    @TargetApi(11)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Fragments inflate their view in onCreateView, not in onCreate
+
+        // Chapter 18 says call super.onCreateView,
+        // but then calling configureAddCrimeButton gives null pointer exception
+        // probably because super doesn't have add_crime_button.
+        // http://forums.bignerdranch.com/viewtopic.php?f=415&t=6364
+        // View rootView = super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_crimelist_list, container, false);
         configureAddCrimeButton(rootView);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (mSubtitleVisible) {
+                getActivity().getActionBar().setSubtitle(R.string.subtitle);
+            }
+        }
+
+        ListView listView = (ListView)rootView.findViewById(android.R.id.list);
+        registerForContextMenu(listView);
+
         return rootView;
     }
 
@@ -124,13 +145,29 @@ public class CrimeListFragment extends ListFragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
+        MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+        if (mSubtitleVisible && showSubtitle != null) {
+            showSubtitle.setTitle(R.string.hide_subtitle);
+        }
     }
 
+    @TargetApi(11)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_item_new_crime:
                 return addCrimeClicked();
+            case R.id.menu_item_show_subtitle:
+                if (getActivity().getActionBar().getSubtitle() == null) {
+                    getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                    mSubtitleVisible = true;
+                    item.setTitle(R.string.hide_subtitle);
+                } else {
+                    getActivity().getActionBar().setSubtitle(null);
+                    mSubtitleVisible = false;
+                    item.setTitle(R.string.show_subtitle);
+                }
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
