62a6bfbf960617a5534e124a0f787ba01b7bb7e0
beepscore <support@beepscore.com>
2014-10-14 23:12:36 -0700

    In CrimeCameraFragment add instance variables and override onCreateView.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
@@ -1,9 +1,37 @@
 package com.beepscore.android.criminalintent;
 
+import android.hardware.Camera;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by stevebaker on 10/14/14.
  */
 public class CrimeCameraFragment extends Fragment {
+    private static final String TAG = "CrimeCameraFragment";
+
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
+
+        Button takePictureButton = (Button)view.findViewById(R.id.crime_camera_takePictureButton);
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
+        mSurfaceView = (SurfaceView)view.findViewById(R.id.crime_camera_surfaceView);
+        return view;
+    }
 }
