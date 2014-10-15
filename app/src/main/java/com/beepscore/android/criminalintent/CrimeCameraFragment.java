673069e5aa40834aa096dc1a08788969116e271d
beepscore <support@beepscore.com>
2014-10-14 22:54:17 -0700

    Add class CrimeCameraFragment.

---
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
@@ -0,0 +1,9 @@
+package com.beepscore.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by stevebaker on 10/14/14.
+ */
+public class CrimeCameraFragment extends Fragment {
+}
