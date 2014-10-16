91fb6fcbb28ae346b765c4663d6a87180defbeef
beepscore <support@beepscore.com>
2014-10-15 23:26:48 -0700

    In CrimeCameraFragment override onResume and onPause

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
@@ -1,6 +1,8 @@
 package com.beepscore.android.criminalintent;
 
+import android.annotation.TargetApi;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,8 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.lang.annotation.Target;
 
 /**
  * Created by stevebaker on 10/14/14.
@@ -34,4 +38,27 @@ public class CrimeCameraFragment extends Fragment {
         mSurfaceView = (SurfaceView)view.findViewById(R.id.crime_camera_surfaceView);
         return view;
     }
+
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // TODO: Don't block main thread while opening camera
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
 }
