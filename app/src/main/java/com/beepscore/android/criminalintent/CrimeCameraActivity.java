d309bb17a8e019a9c53c7f4f224ac75af6206065
beepscore <support@beepscore.com>
2014-10-29 00:10:00 -0700

    In CrimeCameraActivity override onCreate to hide window title and status bar.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraActivity.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraActivity.java
@@ -1,11 +1,28 @@
 package com.beepscore.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by stevebaker on 10/14/14.
  */
 public class CrimeCameraActivity extends SingleFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Hide the window title
+        // Call to requestWindowFeature must be made in activity not fragment
+        // Must call before super.onCreate calls Activity.setContentView
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
