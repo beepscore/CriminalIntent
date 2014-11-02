a09fd0c820c3b3402109740b13d641cd8a347faa
beepscore <support@beepscore.com>
2014-11-01 22:43:29 -0700

    In CrimeCameraFragment add mProgressContainer, mShutterCallback, and mJpegCallback. In takePictureButton OnClickListener onClick call mCamera.takePicture.

--- app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/beepscore/android/criminalintent/CrimeCameraFragment.java
@@ -1,6 +1,7 @@
 package com.beepscore.android.criminalintent;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.os.Build;
@@ -14,17 +15,60 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by stevebaker on 10/14/14.
  */
 public class CrimeCameraFragment extends Fragment {
+
     private static final String TAG = "CrimeCameraFragment";
 
     private Camera mCamera;
     private SurfaceView mSurfaceView;
+    private View mProgressContainer;
+
+    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            // Display the progress indicator
+            mProgressContainer.setVisibility(View.VISIBLE);
+        }
+    };
+
+    private Camera.PictureCallback mJpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // Create a filename
+            String filename = UUID.randomUUID().toString() + ".jpg";
+            // Save the jpeg data to disk
+            FileOutputStream os = null;
+            boolean success = true;
+
+            try {
+                os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+                os.write(data);
+            } catch (Exception e) {
+                Log.e(TAG, "Error writing to file " + filename, e);
+                success = false;
+            } finally {
+                try {
+                    if (os != null) {
+                        os.close();
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "Error closing file " + filename, e);
+                    success = false;
+                }
+            }
+
+            if (success) {
+                Log.i(TAG, "JPEG saved at " + filename);
+            }
+            getActivity().finish();
+        }
+    };
 
     @Override
     @SuppressWarnings("deprecation")
@@ -32,11 +76,16 @@ public class CrimeCameraFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
 
+        mProgressContainer = view.findViewById(R.id.crime_camera_progressContainer);
+        mProgressContainer.setVisibility(View.INVISIBLE);
+
         Button takePictureButton = (Button)view.findViewById(R.id.crime_camera_takePictureButton);
 
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                getActivity().finish();
+                if (mCamera != null) {
+                   mCamera.takePicture(mShutterCallback, null, mJpegCallback);
+                }
             }
         });
 
